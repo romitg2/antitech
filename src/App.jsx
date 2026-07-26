219f2412d3e65447d9c7602b8b54b5094cfc7b90
Romit <romitgabani1@gmail.com>
2026-07-26 21:24:33 +0530

    Move homepage to /home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-import { Canvas } from "@react-three/fiber";
-import { Scroll } from "@react-three/drei";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Content from "./Content/Content";
 import "./App.css";
-import Experience from "./experience/Experience";
-import { Perf } from "r3f-perf";
-import { ScrollControls } from "@react-three/drei";
-import { WhyUs } from "./Content/WhyUs/WhyUs";
 
 function App() {
   return (
-    <>
-      <Content />
-      {/* <Canvas> */}
-      {/*   <ScrollControls pages={5}> */}
-      {/*     <Scroll html></Scroll> */}
-      {/*     <Scroll> */}
-      {/*       <Perf position="top-right" /> */}
-      {/*       <Experience /> */}
-      {/*     </Scroll> */}
-      {/*   </ScrollControls> */}
-      {/* </Canvas> */}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
